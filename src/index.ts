a1fc9cee183a3d710706f090fafa8f4bc4a7f551
José Ángel Marrero Domínguez <alu0101494500@ull.edu.es>
2024-05-09 17:58:50 +0000

    Index con todos los routers

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,16 @@ import express from 'express';
 import './db/mongoose.js';
 import { customerRouter } from './routers/customers.js';
 import { defaultRouter } from './routers/default.js';
+import { providerRouter } from './routers/providers.js';
+import { furnitureRouter } from './routers/furnitures.js';
+import { transactionRouter } from './routers/transactions.js';
 
 const app = express();
 app.use(express.json());
 app.use(customerRouter);
+app.use(providerRouter);
+app.use(furnitureRouter);
+app.use(transactionRouter);
 app.use(defaultRouter);
 
 const port = process.env.PORT || 3000;
