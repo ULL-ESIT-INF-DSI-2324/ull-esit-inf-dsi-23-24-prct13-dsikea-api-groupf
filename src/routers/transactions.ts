1dab0b8dfa48d6e79f1fc194ec09df99a5c52912
José Ángel Marrero Domínguez <alu0101494500@ull.edu.es>
2024-05-09 20:42:58 +0000

    Pruebas

--- src/routers/transactions.ts
+++ src/routers/transactions.ts
@@ -53,22 +53,22 @@ transactionRouter.post('/transactions', async (req, res) => {
       try {
         await newFurniture.save();
       } catch (e) {
-        return res.status(400).send(e);
+        return res.status(400).send('Soy yo');
       }
     } else if (!furnitureModel) {
       return res.status(404).send('Furniture not found');
     } else {
       if (type === 'Purchase Order' || type === 'Refund from client') {
-        furnitureModel!.stock += item.quantity;
+        furnitureModel.stock += item.quantity;
       } else {
         if (furnitureModel.stock < item.quantity) {
           return res.status(400).send('Not enough stock');
         }
-        furnitureModel!.stock -= item.quantity;
+        furnitureModel.stock -= item.quantity;
       }
       totalAmount += furnitureModel.price * item.quantity;
       try {
-        await furnitureModel!.save();
+        await furnitureModel.save();
       } catch (e) {
         return res.status(400).send(e);
       }
