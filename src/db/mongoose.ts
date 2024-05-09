109d6a8f5251a76081d2a8a6cec7049509c43ad1
José Ángel Marrero Domínguez <alu0101494500@ull.edu.es>
2024-05-09 13:13:04 +0000

    Comenzando los routers

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -1,7 +1,8 @@
 import { connect } from 'mongoose';
 
 try {
-  await connect('mongodb+srv://dsikea:APIRestDSIkea@cluster0.mff29th.mongodb.net/');
+  //await connect('mongodb+srv://dsikea:APIRestDSIkea@cluster0.mff29th.mongodb.net/');
+  await connect('mongodb+srv://dsikea:APIRestDSIkea@cluster0.mff29th.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
   console.log('Connection to MongoDB server established');
 } catch (error) {
   console.log(error);
--- src/routers/customers.ts
+++ src/routers/customers.ts
@@ -1,3 +1,54 @@
 import express from 'express';
 
+import { Customer } from '../models/customer.js';
+
 export const customerRouter = express.Router();
+
+/**
+ * @swagger
+ * /customers:
+ *  post: 
+ *   summary: Create a new customer
+ */
+customerRouter.post('/customers', async (req, res) => {
+  const customer = new Customer(req.body);
+  try {
+    await customer.save();
+    res.status(201).send(customer);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+/**
+ * @swagger
+ * /customers:
+ *  get:
+ *   summary: Get all customers
+ */
+customerRouter.get('/customers', async (_, res) => {
+  try {
+    const customers = await Customer.find();
+    res.status(201).send(customers);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+/**
+ * @swagger
+ * /customers/{id}:
+ *  get:
+ *   summary: Get a customer by NIF
+ */
+customerRouter.get('/customers/:id', async (req, res) => {
+  try {
+    const customer = await Customer.findById(req.query.nif?.toString());
+    if (!customer) {
+      return res.status(404).send();
+    }
+    res.status(201).send(customer);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
