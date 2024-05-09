df7f53a874c26d8247d321bcba5f36177c72518c
José Ángel Marrero Domínguez <alu0101494500@ull.edu.es>
2024-05-09 20:52:12 +0000

    Pruebas

--- src/routers/transactions.ts
+++ src/routers/transactions.ts
@@ -41,37 +41,33 @@ transactionRouter.post('/transactions', async (req, res) => {
   let newFurniture;
   for (const item of furniture) {
     const furnitureFilter = { name: item.name };
-    const furnitureModel = await Furniture.findOne(furnitureFilter);
-    if (!furnitureModel && (type === 'Purchase Order' || type === 'Refund from client')) {
-      newFurniture = new Furniture({
-        name: item.name,
-        description: item.description,
-        color: item.color,
-        price: item.price,
-        stock: item.quantity
-      });
-      try {
+    try {
+      const furnitureModel = await Furniture.findOne(furnitureFilter);
+      if (!furnitureModel && (type === 'Purchase Order' || type === 'Refund from client')) {
+        newFurniture = new Furniture({
+          name: item.name,
+          description: item.description,
+          color: item.color,
+          price: item.price,
+          stock: item.quantity
+        });
         await newFurniture.save();
-      } catch (e) {
-        return res.status(400).send(e);
-      }
-    } else if (!furnitureModel) {
-      return res.status(404).send('Furniture not found');
-    } else {
-      if (type === 'Purchase Order' || type === 'Refund from client') {
-        furnitureModel.stock += item.quantity;
+      } else if (!furnitureModel) {
+        return res.status(404).send('Furniture not found');
       } else {
-        if (furnitureModel.stock < item.quantity) {
-          return res.status(400).send('Not enough stock');
+        if (type === 'Purchase Order' || type === 'Refund from client') {
+          furnitureModel.stock += item.quantity;
+        } else {
+          if (furnitureModel.stock < item.quantity) {
+            return res.status(400).send('Not enough stock');
+          }
+          furnitureModel.stock -= item.quantity;
         }
-        furnitureModel.stock -= item.quantity;
-      }
-      totalAmount += furnitureModel.price * item.quantity;
-      try {
+        totalAmount += furnitureModel.price * item.quantity;
         await furnitureModel.save();
-      } catch (e) {
-        return res.status(400).send('soy yo');
       }
+    } catch (e) {
+      return res.status(500).send(e);
     }
   }
 
