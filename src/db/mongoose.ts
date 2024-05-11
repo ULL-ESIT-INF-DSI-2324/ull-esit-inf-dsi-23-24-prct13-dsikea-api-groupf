27617c3348709cce1c407ed8f9010cf4252e13ab
Jose <alu0101494500@ull.edu.es>
2024-05-11 18:45:04 +0100

    Cambios config

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -2,7 +2,7 @@ import { connect } from 'mongoose';
 
 try {
   //await connect('mongodb+srv://dsikea:APIRestDSIkea@cluster0.mff29th.mongodb.net/');
-  await connect('mongodb+srv://dsikea:APIRestDSIkea@cluster0.mff29th.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+  await connect(process.env.MONGODB_URL!);
   console.log('Connection to MongoDB server established');
 } catch (error) {
   console.log(error);
